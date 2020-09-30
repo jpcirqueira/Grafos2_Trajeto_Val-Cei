e9c6d3181ea815329386e8de7ec0ab7722593801
João Soares <jpcirqueira81@gmail.com>
2020-09-30 16:04:43 -0300

    adicionado Dijkstra.

---
+++ dijkstra.py
@@ -0,0 +1,125 @@
+grafo = { "A" : { "B" : 1, "C":2 },
+          "B" : { "D":2, "E":4 },
+          "C" : { "E":2 },
+          "D" : { "F": 6 },
+          "E" : { "F": 7 },
+          "F" : { }
+          }
+
+def dijkstra(grafo, origem): #retorna a menor distancia de um dado nó para todos os outros possíveis.
+
+    controle = { }
+    distanciaAtual = { }
+    noAtual = { }
+    naoVisitados = []
+    atual = origem
+    noAtual[atual] = 0
+
+    
+    for vertice in grafo.keys():
+        naoVisitados.append(vertice) #inclui os vertices nos não visitados    
+        distanciaAtual[vertice] = float('inf') #inicia os vertices como infinito
+
+    distanciaAtual[atual] =0
+
+    naoVisitados.remove(atual)
+
+    while naoVisitados:
+        for vizinho, peso in grafo[atual].items():
+             pesoCalc = peso + noAtual[atual]
+             if distanciaAtual[vizinho] == float("inf") or distanciaAtual[vizinho] > pesoCalc:
+                 distanciaAtual[vizinho] = pesoCalc
+                 controle[vizinho] = distanciaAtual[vizinho]
+
+        if controle == {} : break    
+        minVizinho = min(controle.items(), key=lambda x: x[1]) #seleciona o menor vizinho
+        atual=minVizinho[0]
+        noAtual[atual] = minVizinho[1]
+        naoVisitados.remove(atual)
+        del controle[atual]
+
+    print(distanciaAtual)
+
+
+def dijkstra_distancia(grafo, origem, fim): #retorna a menor distancia de um No origem até um No destino e o caminho até ele
+
+    controle = { }
+    distanciaAtual = { }
+    noAtual = { }
+    naoVisitados = []
+    atual = origem
+    noAtual[atual] = 0
+
+    
+    for vertice in grafo.keys():
+        naoVisitados.append(vertice) #inclui os vertices nos não visitados    
+        distanciaAtual[vertice] = float('inf') #inicia os vertices como infinito
+
+    distanciaAtual[atual] = [0,origem] 
+
+    naoVisitados.remove(atual)
+
+    while naoVisitados:
+        for vizinho, peso in grafo[atual].items():
+             pesoCalc = peso + noAtual[atual]
+             if distanciaAtual[vizinho] == float("inf") or distanciaAtual[vizinho][0] > pesoCalc:
+                 distanciaAtual[vizinho] = [pesoCalc,atual]
+                 controle[vizinho] = pesoCalc
+                 print(controle)
+                 
+        if controle == {} : break    
+        minVizinho = min(controle.items(), key=lambda x: x[1]) #seleciona o menor vizinho
+        atual=minVizinho[0]
+        noAtual[atual] = minVizinho[1]
+        naoVisitados.remove(atual)
+        del controle[atual]
+
+    print("A menor distância da rua  %s até %s é: %s" % (origem, fim, distanciaAtual[fim][0]))
+    print("O menor caminho é: %s" % printPath(distanciaAtual,origem, fim))          
+    
+def dijkstra_tempo(grafo, origem, fim): #retorna a menor tempo
+
+    controle = { }
+    distanciaAtual = { }
+    noAtual = { }
+    naoVisitados = []
+    atual = origem
+    noAtual[atual] = 0
+
+    
+    for vertice in grafo.keys():
+        naoVisitados.append(vertice)    
+        distanciaAtual[vertice] = float('inf') 
+
+    distanciaAtual[atual] = [0,origem] 
+
+    naoVisitados.remove(atual)
+
+    while naoVisitados:
+        for vizinho, peso in grafo[atual].items():
+             pesoCalc = peso + noAtual[atual]
+             if distanciaAtual[vizinho] == float("inf") or distanciaAtual[vizinho][0] > pesoCalc:
+                 distanciaAtual[vizinho] = [pesoCalc,atual]
+                 controle[vizinho] = pesoCalc
+                 print(controle)
+                 
+        if controle == {} : break    
+        minVizinho = min(controle.items(), key=lambda x: x[1])
+        atual=minVizinho[0]
+        noAtual[atual] = minVizinho[1]
+        naoVisitados.remove(atual)
+        del controle[atual]
+
+    print("A menor tempo da rua  %s para a rua%s é: %s" % (origem, fim, distanciaAtual[fim][0]))
+    ## print("O menor tempo é: %s" % print_distancia(distanciaAtual,origem, fim))          
+    
+
+def print_distancia(distancias,inicio, fim):
+        if  fim != inicio:
+            return "%s -- %s" % (print_distancia(distancias,inicio, distancias[fim][1]),fim)
+        else:
+            return inicio
+
+
+##dijkstra(grafo, "A")
+##dijkstra_path(grafo, "A", "F")
